8793683376eb2ea78797f259839a154dd053b197
Kate Guseva <Marzia240015@gmail.com>
2023-02-10 15:46:57 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import "./App.css";
-import Steps from "./Components/Steps/Steps";
+import BasicCard from "./Components/BasicCard/BasicCard";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <Steps />;
+  const params = {
+    title: "Card Title",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    textButton: "Go somewhere",
+    img: "http://placehold.it/180x100",
+  };
+
+  const params2 = {
+    title: "Card Title",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    textButton: "Go somewhere",
+    img: "",
+  };
+
+  return (
+    <div>
+      <BasicCard {...params} />
+    </div>
+  );
 }
 
 export default App;
