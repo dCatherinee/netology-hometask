0abfcae6abad89ec07ca218a81f97780c8817a87
Kate Guseva <Marzia240015@gmail.com>
2023-02-28 17:00:19 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,77 +1,68 @@
-import { useState, useEffect } from "react";
-import { RxUpdate } from "react-icons/rx";
-import Notes from "./Components/Notes/Notes";
-import Form from "./Components/Form/Form";
-import { getNotes, postNotes, deleteNotes } from "./api";
+import React, { useState } from "react";
+import getPrettyDate from "./utils/utils";
 import "./App.css";
 
-const URL = "http://localhost:7777/notes";
+function withDateTime(Component, date) {
+  return function (props) {
+    const prettyDate = getPrettyDate(date);
 
-function App() {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState(false);
-  const [noteDelete, setNoteDelete] = useState(false);
-  const [update, setUpdate] = useState(false);
-
-  useEffect(() => {
-    getNotes(URL)
-      .then((data) => setNotes(data))
-      .catch((e) => console.log(e));
-  }, []);
-
-  useEffect(() => {
-    getNotes(URL)
-      .then((data) => setNotes(data))
-      .catch((e) => console.log(e));
-
-    setNewNote(false);
-  }, [newNote]);
-
-  useEffect(() => {
-    getNotes(URL)
-      .then((data) => setNotes(data))
-      .catch((e) => console.log(e));
-
-    setNoteDelete(false);
-  }, [noteDelete]);
-
-  useEffect(() => {
-    getNotes(URL)
-      .then((data) => setNotes(data))
-      .catch((e) => console.log(e));
-
-    setUpdate(false);
-  }, [update]);
-
-  const addNote = (note) => {
-    postNotes(URL, note)
-      .then((data) => console.log(data))
-      .catch((e) => console.log(e));
-    setNewNote(true);
+    return <Component {...props} date={prettyDate} />;
   };
+}
 
-  const deleteNote = (id) => {
-    deleteNotes(URL, id)
-      .then((data) => console.log(data))
-      .catch((e) => console.log(e));
+function DateTime(props) {
+  return <p className="date">{props.date}</p>;
+}
 
-    setNoteDelete(true);
-  };
-
-  const updateNotes = () => {
-    setUpdate(true);
-  };
+function Video(props) {
+  const PrettyDate = withDateTime(DateTime, props.date);
 
   return (
-    <div className="container">
-      <div className="header">
-        <h1 className="header__title">Notes</h1>
-        <RxUpdate onClick={updateNotes} className="header__icon" />
-      </div>
-      <Notes notes={notes} onDelete={deleteNote} />
-      <Form onAdd={addNote} />
+    <div className="video">
+      <iframe
+        src={props.url}
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      ></iframe>
+      <PrettyDate />
     </div>
   );
 }
 
-export default App;
+function VideoList(props) {
+  return props.list.map((item, id) => (
+    <Video key={id} url={item.url} date={item.date} />
+  ));
+}
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2017-07-31 13:24:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2018-03-03 12:10:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2018-02-03 23:16:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2018-01-03 12:10:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2018-01-01 16:17:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2017-12-02 05:24:00",
+    },
+  ]);
+
+  return <VideoList list={list} />;
+}
